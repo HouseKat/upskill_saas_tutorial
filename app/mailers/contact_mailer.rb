49b23ba42ffad2ed1f0ae91bf2d54a023fc5457e
John C <jahnone.clement@gmail.com>
2016-11-30 22:00:29 +0000

    Added mailer settings to environment file

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'poppyeyes@openmailbox.org'
+  default to: 'jahnone.3000@gmail.com'
   
   def contact_email(name, email, body)
     @name = name
